28e54507da5684d197dbedaa48e0b0ed7285aba0
Oleh Kurpiak <oleh.kurpiak@gmail.com>
2017-12-26 11:38:58 +0200

    compare persons

--- src/main/java/borsch/facedetection/web/controllers/IndexController.java
+++ src/main/java/borsch/facedetection/web/controllers/IndexController.java
@@ -1,7 +1,10 @@
 package borsch.facedetection.web.controllers;
 
-import org.opencv.core.CvType;
-import org.opencv.core.Mat;
+import org.opencv.core.*;
+import org.opencv.features2d.DescriptorExtractor;
+import org.opencv.features2d.DescriptorMatcher;
+import org.opencv.features2d.FeatureDetector;
+import org.opencv.imgcodecs.Imgcodecs;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,8 +29,6 @@ public class IndexController {
 
         nu.pattern.OpenCV.loadShared();
         System.loadLibrary(org.opencv.core.Core.NATIVE_LIBRARY_NAME);
-        Mat mat = Mat.eye(3, 3, CvType.CV_8UC1);
-        System.out.println("mat = " + mat.dump());
     }
 
     @RequestMapping(value = {"/", "/index"}, method = RequestMethod.GET)
@@ -61,6 +62,42 @@ public class IndexController {
         return true;
     }
 
+    @RequestMapping(value = "/person/check", method = RequestMethod.POST)
+    public @ResponseBody boolean checkIfPersonExists(
+            @RequestParam(name = "file") MultipartFile file
+    ) {
+        File localFile = new File(TEMP_FOR_REQUEST + "/" + file.getOriginalFilename());
+        try {
+            file.transferTo(localFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+
+            return false;
+        }
+
+        try {
+            File[] persons = new File(ROOT_DIR).listFiles();
+            if (persons == null) {
+                return false;
+            }
+
+            for (File person : persons) {
+                if (compareImages(localFile.getAbsolutePath(), person.getAbsolutePath())) {
+                    knownPerson = true;
+
+                    return true;
+                }
+            }
+
+            return false;
+        } finally {
+            if (localFile.exists()) {
+                localFile.setWritable(true);
+                localFile.delete();
+            }
+        }
+    }
+
     @RequestMapping(value = "/person/image", method = RequestMethod.GET)
     public void getPersonImage(
             @RequestParam("name") String name,
@@ -91,6 +128,58 @@ public class IndexController {
 
         return true;
     }
+
+    @RequestMapping(value = "/person/enter", method = RequestMethod.GET)
+    public @ResponseBody boolean canPersonEnter() {
+        try {
+            return knownPerson;
+        } finally {
+            knownPerson = false;
+        }
+    }
+
+    private boolean compareImages(String path1, String path2) {
+        FeatureDetector detector = FeatureDetector.create(FeatureDetector.ORB);
+        DescriptorExtractor descriptor = DescriptorExtractor.create(DescriptorExtractor.ORB);
+
+        DescriptorMatcher matcher = DescriptorMatcher.create(DescriptorMatcher.BRUTEFORCE_HAMMING);
+
+        // first image
+        Mat img1 = Imgcodecs.imread(path1, Imgcodecs.CV_LOAD_IMAGE_COLOR);
+        Mat descriptors1 = new Mat();
+        MatOfKeyPoint keypoints1 = new MatOfKeyPoint();
+
+        detector.detect(img1, keypoints1);
+        descriptor.compute(img1, keypoints1, descriptors1);
+
+        // second image
+        Mat img2 = Imgcodecs.imread(path2, Imgcodecs.CV_LOAD_IMAGE_COLOR);
+        Mat descriptors2 = new Mat();
+        MatOfKeyPoint keypoints2 = new MatOfKeyPoint();
+
+        detector.detect(img2, keypoints2);
+        descriptor.compute(img2, keypoints2, descriptors2);
+
+        // match these two keypoints sets
+        MatOfDMatch matches = new MatOfDMatch();
+        matcher.match(descriptors1, descriptors2, matches);
+
+        float total = 0;
+
+        for (DMatch m : matches.toArray()) {
+            // how to use these values to detect the similarity? They seem to be way off
+            // all of these values are in range 50-80 which seems wrong to me
+            total += m.distance;
+        }
+
+        float averageDistance = total / matches.total();
+
+        System.out.println(path2 + ", distance score: " + averageDistance + ", commons in %: " + (100 - averageDistance) + "%");
+
+        return averageDistance <= 50f;
+    }
+
+    private static boolean knownPerson = false;
 
     private String ROOT_DIR = System.getProperty("catalina.home") + "/temp/face_detection_saved";
     private String TEMP_FOR_REQUEST = System.getProperty("catalina.home") + "/temp/temp_requests";
